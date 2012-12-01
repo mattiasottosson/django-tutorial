3230fc4ec27a0fb7fef4015a3fb34f786352f2dc
Mattias Ottosson <himself@mattiasottosson.se>
2012-12-01 13:28:25 +0100

    Replace custom views with generic ones.  Step 4 completed.

--- polls/views.py
+++ polls/views.py
@@ -3,19 +3,6 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from polls.models import Choice, Poll
 
-def index(request):
-  latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-  context = {'latest_poll_list': latest_poll_list}
-  return render(request, 'polls/index.html', context)
-
-def detail(request, poll_id):
-  poll = get_object_or_404(Poll, pk=poll_id)
-  return render(request, 'polls/detail.html', {'poll': poll})
-
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll': poll})
-
 def vote(request, poll_id):
   p = get_object_or_404(Poll, pk=poll_id)
   try:
